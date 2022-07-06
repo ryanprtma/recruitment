7f5ba831322d35822b63c2a37281a031db20b8cc
Ryan Apratama <ryanapratama16@gmail.com>
2022-07-06 21:03:59 +0700

    update taskcontroler

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Dentro\Yalr\Attributes\Get;
+use Dentro\Yalr\Attributes\Middleware;
 
 class TaskController extends Controller
 {
+    #[Get('tasks')]
     public function index(Request $request){
         if($request->search){
             $task =Task::where('task', 'LIKE', "%$request->search%")
